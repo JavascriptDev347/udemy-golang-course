3973f02cba0055910dde48fb805d409c915565ee
RustambekJavascript <rustambekeshboriyev@gmail.com>
2026-05-02 10:42:55 +0500

    foor-loop

--- section_2/1-foor-loop/main.go
+++ section_2/1-foor-loop/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+func main() {
+
+	a := 10
+	for i := 1; i < 10; i++ {
+		a *= i
+		println(a)
+	}
+
+	// while style loop
+	k := 3
+	for k > 0 {
+		println(k)
+		k--
+	}
+}
