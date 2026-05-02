c739443a30a4dbe7e886ff5e7229f3459572cbc8
RustambekJavascript <rustambekeshboriyev@gmail.com>
2026-05-02 11:00:58 +0500

    if-else

--- section_2/2-if-else/main.go
+++ section_2/2-if-else/main.go
@@ -1,3 +1,35 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	tmp := 25
+	if tmp > 30 {
+		fmt.Println("HOT")
+	} else {
+		fmt.Println("NORM")
+	}
+
+	score := 87
+	if score >= 90 {
+		fmt.Println("A")
+	} else if score >= 80 {
+		fmt.Println("B")
+	} else if score >= 70 {
+		fmt.Println("C")
+	} else {
+		fmt.Println("Failed")
+	}
+
+	userAccess := map[string]bool{
+		"Russel": true,
+		"James":  true,
+		"Goose":  false,
+	}
+
+	if hasAccess, ok := userAccess["Russel"]; ok {
+		if hasAccess {
+			fmt.Println(hasAccess, ok)
+		}
+	}
+}
