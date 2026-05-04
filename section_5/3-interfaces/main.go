b5a9fb61ceb21dfd4664186f58b9ceb318e5b839
RustambekJavascript <rustambekeshboriyev@gmail.com>
2026-05-04 20:23:31 +0500

    interfaces

---
+++ section_5/3-interfaces/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Person interface {
+	GetName() string
+	GetID() int
+}
+
+type Employee struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Position  string
+	Salary    int
+	IsActive  bool
+	JoinedAt  time.Time
+}
+
+type BusinessEmployee struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Position  string
+	Salary    int
+	IsActive  bool
+}
+
+func (b BusinessEmployee) GetName() string {
+	return b.FirstName + " " + b.LastName
+}
+
+func (e Employee) GetName() string {
+	return e.FirstName + " " + e.LastName
+}
+
+func displayName(p Person) {
+	fmt.Println(p.GetName())
+}
+
+func main() {
+	joe := Employee{
+		ID:        1,
+		FirstName: "Joe",
+		LastName:  "Smith",
+		Position:  "Vibe coder",
+		Salary:    9000,
+		IsActive:  true,
+		JoinedAt:  time.Now(),
+	}
+
+	displayName(joe)
+
+	b := BusinessEmployee{
+		ID:        1,
+		FirstName: "Bob",
+		LastName:  "Smith",
+		Position:  "Vibe coder",
+		Salary:    9000,
+		IsActive:  true,
+	}
+
+	displayName(b)
+}
