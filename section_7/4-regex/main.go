8bbf6234e7da26bf512206f5b52f2b7476df2626
RustambekJavascript <rustambekeshboriyev@gmail.com>
2026-05-05 22:16:04 +0500

    regex and project

---
+++ section_7/4-regex/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func main() {
+	text1 := "Hello world! Welcome to Go"
+	regGo, err := regexp.Compile(`Go`)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1) // stop the code
+	}
+
+	fmt.Printf("Text '%s', matches 'Go': %t\n", text1, regGo.MatchString(text1))
+}
