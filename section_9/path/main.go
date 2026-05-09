4cec44bc49b4faf8f3aba5aafe8baa0628799166
RustambekJavascript <rustambekeshboriyev@gmail.com>
2026-05-09 19:34:42 +0500

    paths

---
+++ section_9/path/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+)
+
+func main() {
+	fmt.Println("Path learning in go")
+	// Join for add files each other
+	path1 := filepath.Join("C:", "Users", "John", "Documents", "file.txt")
+	fmt.Println(path1)
+
+	path2 := filepath.Join("config", "app.yml")
+	fmt.Println(path2)
+
+	fmt.Println("Base:", filepath.Base(path1)) // Get the base name of the path}
+	fmt.Println("Ext:", filepath.Ext(path1))
+
+	dirtyDir := "./users/./dir/../other_dir/./file.txt"
+	fmt.Println(filepath.Clean(dirtyDir)) // Clean
+}
